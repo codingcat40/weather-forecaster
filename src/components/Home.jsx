c32b53b3ec89f37ac74e5cc06124049c9737473e
Naman Matoliya <naman@fieldproxy.com>
2025-06-03 12:56:11 +0530

    fetch current location

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-  return (
-    <div className='text-center text-3xl'>Home</div>
-  )
-}
+  const [location, setLocation] = useState({ latitude: "", longitude: "" });
+  const fetchCurrentLocation = () =>  {
+    navigator.geolocation.watchPosition((pos) => {
+          setLocation({latitude: pos.coords.latitude, longitude:  pos.coords.longitude});
+      })
 
-export default Home
+  }
+
+  useEffect(() => {
+    fetchCurrentLocation()
+  }, [])
+  return (
+    <div className="fixed top-20 w-full flex justify-center z-10 px-4">
+      <div className="text-center max-w-2xl">
+        <h1 className="text-3xl font-semibold mb-2">Your Location: Longitude: {location.longitude}, Latitude:  {location.latitude}</h1>
+
+        <p className="text-sm text-gray-700">
+          {/* search button here probably */}
+          {/* then todays weather or forecase */}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Link } from 'react-router'
-import AboutAPI from './AboutAPI'
-
+import React from "react";
+import { Link } from "react-router";
 
 const Navbar = () => {
   return (
-    <nav className='sticky z-50'>
-        <Link to='/about-api'>About API Route</Link>
+    <nav className="fixed top-0 w-full h-12 bg-red-300/20 flex items-center justify-end px-4 z-50">
+      <Link to="/about-api" className="text-sm text-black hover:underline">
+        About API Route
+      </Link>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
